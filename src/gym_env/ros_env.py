f7f23a469504f1b1dac8d3ebb885d73ee6c29ef7
Alexander Keijzer <alexanderkeijzer@gmail.com>
2021-02-09 22:28:24 +0100

    Non-dictionary spaces for baselines to work

--- src/gym_env/ros_env.py
+++ src/gym_env/ros_env.py
@@ -25,19 +25,23 @@ class ROSEnv(gym.Env):
         self.observations = OrderedDict()
         for sensor in sensors:
             register_sensor = rospy.ServiceProxy(sensor, RegisterSpaces)
-            low, high, topic = register_sensor()
+            rospy.wait_for_service(sensor, 10.0)
+            resp = register_sensor()
 
-            obs_dict[sensor] = spaces.Box(list(low), list(high))
+            obs_dict[sensor] = spaces.Box(np.array(resp.low), np.array(resp.high))
 
-            rospy.Subscriber(topic, SensorMessage, self._sensor_callback)
-            self.observations[topic] = None
+            rospy.Subscriber(resp.topic, SensorMessage, self._sensor_callback)
+            self.observations[resp.topic] = np.zeros(len(resp.low))
+        
         
         if len(obs_dict) == 1:
-            self.observation_space = obs_dict.values()[0]
+            self.observation_space = list(obs_dict.values())[0]
         else:
-            self.observation_space = spaces.Dict(spaces=obs_dict)
+            self.observation_space = spaces.Dict(obs_dict)
+        
+        #self.observation_space = spaces.Dict(obs_dict)
 
-        rospy.loginfo("Observations space: {0}", self.observation_space)
+        rospy.logwarn("Observations space: %s", str(self.observation_space))
         
         # Init Actuators
         rospy.loginfo("Initializing Actuators")
@@ -46,32 +50,43 @@ class ROSEnv(gym.Env):
         self.actuators = OrderedDict()
         for actuator in actuators:
             register_actuator = rospy.ServiceProxy(actuator, RegisterSpaces)
-            low, high, topic = register_actuator()
+            rospy.wait_for_service(actuator, 10.0)
+            resp = register_actuator()
 
-            act_dict[actuator] = spaces.Box(list(low), list(high))
+            act_dict[actuator] = spaces.Box(np.array(resp.low), np.array(resp.high))
 
-            self.actuators[topic] = rospy.Publisher(topic, ActuatorMessage)
+            self.actuators[resp.topic] = rospy.Publisher(resp.topic, ActuatorMessage, queue_size=1)
+        
         
         if len(act_dict) == 1:
-            self.action_space = act_dict.values()[0]
+            self.action_space = list(act_dict.values())[0]
         else:
-            self.action_space = spaces.Dict(spaces=act_dict)
+            self.action_space = spaces.Dict(act_dict)
+        
+        #self.action_space = spaces.Dict(act_dict)
 
-        rospy.loginfo("Action space: {0}", self.action_space)
+        rospy.logwarn("Action space: %s", str(self.action_space))
 
         self.get_reward = rospy.ServiceProxy(reward, GetReward)
-        self.step_env = rospy.ServiceProxy(environment, StepEnv)
-        self.reset_env = rospy.ServiceProxy(environment, ResetEnv)
-        self.close_env = rospy.ServiceProxy(environment, CloseEnv)
+        self.step_env = rospy.ServiceProxy(environment + '/step', StepEnv)
+        self.reset_env = rospy.ServiceProxy(environment + '/reset', ResetEnv)
+        self.close_env = rospy.ServiceProxy(environment + '/close', CloseEnv)
 
     def step(self, action):
 
-        for (topic, act) in action:
-            self.actuators[topic].publish(ActuatorMessage(act))
+        for act in self.actuators.values():
+            if type(self.action_space) == OrderedDict:
+                pass
+            else:
+                act.publish(ActuatorMessage(action))
+
+
+        #for (topic, act) in action:
+        #    self.actuators[topic].publish(ActuatorMessage(act))
 
         # How do reward, done and obs relate to each other? What is the correct order here?
-        done = self.step_env()
-        reward = self.get_reward()
+        done = self.step_env().done
+        reward = self.get_reward().reward
 
         observation = self._get_observations()
 
@@ -92,10 +107,7 @@ class ROSEnv(gym.Env):
         rospy.signal_shutdown("Shutdown requested by Gym")
 
     def _sensor_callback(self, data):
-        self.observations[data._connection_header['topic']] = data.data
+        self.observations[data._connection_header['topic']] = data.observations
     
     def _get_observations(self):
-        obs_list = []
-        for obs in self.observations.values():
-            obs_list += obs
-        return np.array(obs_list)
+        return np.array(list(self.observations.values())).flatten()
